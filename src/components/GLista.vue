bf9c2ec0a5d070581f097081e141024427c416ff
gabriela <gubriana@gmail.com>
2020-07-13 09:26:44 -0400

    add: corrección de error en v-for

--- src/components/GLista.vue
+++ src/components/GLista.vue
@@ -2,11 +2,11 @@
         <div id = "app">
             <h1 v-if = "encabezado_paises_si">Estos son los paises a los que {{ nombre }} quiere viajar</h1>
             <ul>
-                <li v-for = "paisessi in lista_paises_si" v-bind:key >{{paisessi}}</li>
+                <li v-for = "(paisessi, i) in lista_paises_si" v-bind:key="i" >{{paisessi}}</li>
             </ul>
             <h1 v-if= "encabezado_paises_no">Estos son los paises a los que {{ nombre }} no íria</h1>
             <ul>
-                <li v-for = "paisesno in lista_paises_no" v-bind:key >{{paisesno}}</li>
+                <li v-for = "(paisesno, i) in lista_paises_no" v-bind:key="i" >{{paisesno}}</li>
             </ul>
         </div>
 </template>
